b223b35bbb0a3512aa0b40d41780c4f01c44effb
Rainer Beutelmann <rainer.beutelmann@uni-oldenburg.de>
2022-02-21 19:16:06 +0100

    started work on new features for summary figures

---
+++ ABRTraceOrSet.m
@@ -0,0 +1,16 @@
+classdef ABRTraceOrSet < handle
+    
+    properties
+    end
+    
+    methods
+        
+        function obj = ABRTraceOrSet(varargin)
+        end
+        
+        function delete(obj)
+        end
+        
+    end
+    
+end
